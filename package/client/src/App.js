c5fc2976390f0122ccbe91ccca63d6dae372d0cd
Daniella Alamo <dalamov08@gmail.com>
2022-01-26 13:53:52 +0100

    Front structure

---
+++ package/client/src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import {  Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import "./styles/App.scss";
+
+import * as ROUTES from "./routes";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+
+import { onAuthStateChanged } from "./services/auth";
+import { syncSignIn, signOut } from "./redux/auth/auth-actions";
+
+function App() {
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   let unsubscribeFromAuth = null;
+
+  //   unsubscribeFromAuth = onAuthStateChanged((user) => {
+  //     if (user) {
+  //       dispatch(syncSignIn());
+  //     } else {
+  //       dispatch(signOut());
+  //     }
+  //   });
+
+  //   return () => {
+  //     if (unsubscribeFromAuth) {
+  //       unsubscribeFromAuth();
+  //     }
+  //   };
+  // }, [dispatch]);
+
+  return (
+    <div className="App__container">
+      <Routes>
+        <Route path={ROUTES.SIGN_UP} element={SignUp} />
+        <Route path={ROUTES.LOGIN} element={Login} />
+        <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword/>} />
+        <Route path={ROUTES.HOME} element={<Home/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
