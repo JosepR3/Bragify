38841199d080e64513997024e40eb73a4318a985
andarbech <andarbech1.com>
2022-02-03 11:45:09 +0100

    add create

--- package/api/src/controllers/track-controller.js
+++ package/api/src/controllers/track-controller.js
@@ -2,31 +2,32 @@ const { UserRepo, TrackRepo } = require("../repositories");
 const { handleDbResponse } = require("../repositories/repo-utils");
 
 async function createTrack(req, res, next) {
-  // console.log("Inside")
-  // const {
-  //   body: { title, url, thumbnail, genre, duration = 0 },
-  //   user: { uid },
-  // } = req;
+  console.log(req.body)
+  
+  const {
+    body: { title, url, thumbnail, genre, duration  },
+    // user: { uid },
+  } = req;
 
   try {
-    //   // if (!title && !url) {
-    //   //   res.status(400).send({
-    //   //     data: null,
-    //   //     error: "Missing Fields (title, url)",
-    //   //   });
-    //   // }
+      if (!title && !url) {
+        res.status(400).send({
+          data: null,
+          error: "Missing Fields (title, url)",
+        });
+      }
 
-    //   // const user = await UserRepo.findOne({
-    //   //   firebase_id: "ge2WtP90RRV4udE7OMvNGmWsYl43",
-    //   // });
+      // const user = await UserRepo.findOne({
+      //   firebase_id: "ge2WtP90RRV4udE7OMvNGmWsYl43",
+      // });
 
     const dbResponse = await TrackRepo.create({
-      title: "title",
-      url: "url",
-      thumbnail: "thumbnail",
-      duration: 0,
-      genre: "pop",
-      authorId: "ge2WtP90RRV4udE7OMvNGmWsYl43",
+      title: title,
+      url: url,
+      thumbnail: thumbnail,
+      duration: duration,
+      genre: genre,
+      authorId: "Andresito",
     });
 
     handleDbResponse(res, dbResponse);
