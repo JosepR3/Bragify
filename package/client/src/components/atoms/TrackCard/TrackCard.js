51ae38a53b019c56aa3dc3cc0d857b619c6da638
CYBER-MARCUSSEN <83762741+CYBER-MARCUSSEN@users.noreply.github.com>
2022-02-03 16:02:19 +0100

    song card in progress

--- package/client/src/components/atoms/TrackCard/TrackCard.js
+++ package/client/src/components/atoms/TrackCard/TrackCard.js
@@ -1,79 +1,61 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import PlayArrowSharpIcon from '@mui/icons-material/PlayArrowSharp';
-import PauseSharpIcon from '@mui/icons-material/PauseSharp';
+// import { useState, useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
 
 // import { playSong } from '../../../api/mock-apis';
 // import { pauseSong } from '../../../api/mock-apis';
 
-import { playTrack } from '../../../redux/tracks/tracks-actions';
+// import { playTrack } from '../../../redux/tracks/tracks-actions';
 
-import './TrackCard.scss';
+import "./TrackCard.scss";
 
-export default function TrackCard({ albumName,
-    artistName,
-    tracks,
-    albumImageUrl }) {
-    const theme = useTheme();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    // const tracks = useSelector(state => state.tracks);
-    // const { currentTrack } = tracks;
-    // const { currentTrack } = tracks;
-    const [isPlaying, setIsPlaying] = useState(false);
+export default function TrackCard() {
+  // { albumName,
+  // artistName,
+  // tracks,
+  // albumImageUrl }
+  // const theme = useTheme();
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // // const tracks = useSelector(state => state.tracks);
+  // // const { currentTrack } = tracks;
+  // // const { currentTrack } = tracks;
+  // const [isPlaying, setIsPlaying] = useState(false);
 
-    const handleClick = () => {
+  // const handleClick = () => {
 
-        dispatch(playTrack(tracks));
+  //     dispatch(playTrack(tracks));
 
+  // };
 
-    };
+  // const viewTracks = (e) => {
+  //     if (e.target.id !== 'pause-button' && e.target.id !== 'play-button' && e.target.id !== 'button-box') {
+  //         navigate('/tracks');
+  //         dispatch(playTrack(tracks));
+  //         // navigate('/track-song-list');
 
-    const viewTracks = (e) => {
-        if (e.target.id !== 'pause-button' && e.target.id !== 'play-button' && e.target.id !== 'button-box') {
-            navigate('/tracks');
-            dispatch(playTrack(tracks));
-            // navigate('/track-song-list');
-
-        }
-    }
-
+  //     }
+  // }
 
     return (
-        <Card sx={{ display: 'flex', bgcolor: '#424242', padding: "10px", margin: '10px', borderRadius: '10%' }} >
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-
-                <CardContent sx={{ flex: '1 0 auto' }}>
-                    {albumName && <Typography component="div" variant="h7" color="#ffebee">
-                        {albumName || " "}
-                    </Typography>}
-                    {artistName && <Typography variant="subtitle1" color="#ffebee" component="div">
-                        {artistName || " "}
-                    </Typography>}
-                </CardContent>
-                <CardMedia
-                    onClick={viewTracks}
-                    component="img"
-                    sx={{ width: '100%', height: '100%', backgroundColor: '#424242', position: 'relative', }}
-                    image={albumImageUrl}
-                    alt="Live from space album cover"
-
-                />
-                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }} id="button-box">
-
-                    {isPlaying ? <PauseSharpIcon style={{ fill: "white" }} onClick={handleClick} id="pause-button" /> : <PlayArrowSharpIcon style={{ fill: "white" }} onClick={handleClick} id="play-button" />}
-
-                </Box>
-            </Box>
-        </Card>
+    <Card className="track_card_container justify-content-center rounded">
+        <Card.Img
+        className="track_card_container_image"
+        variant="top"
+        src="https://images.pexels.com/photos/9420580/pexels-photo-9420580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        >
+        </Card.Img>
+        <Card.Body className="flex-column d-flex justify-content-center">
+        <div className="d-flex gap-3 justify-content-between">
+        <button variant="primary" className="track_card_container_button">▶</button>
+        </div>
+        <Card.Title className="fs-5">Song</Card.Title>
+        <Card.Text className="fs-6">Album</Card.Text>
+        </Card.Body>
+    </Card>
     );
 }
