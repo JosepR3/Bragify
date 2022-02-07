8a18fee0e14f04e01a21c9213a917f8edba202c8
andarbech <andarbech1.com>
2022-02-07 18:36:34 +0100

    finish button Like

--- package/client/src/components/atoms/LikeButton/LikeButton.js
+++ package/client/src/components/atoms/LikeButton/LikeButton.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import Button from 'react-bootstrap/Button';
-import { likeTrack, unlikeTrack, fetchLikedTracks } from '../../../redux/tracks/tracks-actions';
+import { likeTrack, unlikeTrack} from '../../../redux/tracks/tracks-actions';
 import './likeButton.scss';
 import filledLike from '../../../assets/images/filled_like.svg';
 import unfilledLike from '../../../assets/images/unfilled_like.svg';
@@ -11,16 +10,9 @@ export default function LikeButton( {trackId} ) {
     const dispatch = useDispatch();
     const currentUser = JSON.parse(localStorage.getItem('user'))
     const userId=currentUser._id
-    // const userId = useSelector((state)=>state.auth.username.id)
-console.log(currentUser)
     const likedTracks = useSelector((state) => state.tracks.likedTracks);
     const isLiked = likedTracks.find(e => e === trackId);
-    console.log(userId)
-    useEffect(() => {
-        dispatch(fetchLikedTracks(userId));
-    }, [dispatch]);
-    
-    
+
     const handleLike = () => {
         
         if (isLiked) {
